716dcf4420d508ca36cd31b17be93496274e69bf
PotatoJisus <112276251+PotatoJisus@users.noreply.github.com>
2023-02-15 15:23:26 +0300

    2

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import math
 a = int(input("Введите значения числа a: "))
-b = int( input("Введите значения числа b: "))
-c = int( input("Введите значение числа c: "))
+b = int(input("Введите значения числа b: "))
+c = int(input("Введите значение числа c: "))
 D = b**2-4*a*c
 print("Дискриминант равен: ", D)
-if D <0:
+if D < 0:
     print("Корней нет")
 if D == 0:
     x1 = -b/(2*a)
     print("Корень только один")
 if D > 0:
-    x1 = (-b + math.sqrt(D)) / (2 * a)
-    x2 = (b + math.sqrt(D)) / (2 * a)
+    x1 = (-b - math.sqrt(D)) / (2 * a)
+    x2 = (-b + math.sqrt(D)) / (2 * a)
     print("Корня целых два", x1, x2)

23f37c9987179f7b97b4a8af83ddea82fff4d16e
PotatoJisus <112276251+PotatoJisus@users.noreply.github.com>
2023-05-13 02:42:24 +0300

    4

--- lab7/Simple-bot.py
+++ lab7/Simple-bot.py
@@ -21,19 +21,28 @@ cur = con.cursor()
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(row_width=2)
-    itembtn1 = types.KeyboardButton('Понедельник')
-    itembtn2 = types.KeyboardButton('Вторник')
-    itembtn3 = types.KeyboardButton('Среда')
-    itembtn4 = types.KeyboardButton('Четверг')
-    itembtn5 = types.KeyboardButton('Пятница')
-    itembtn6 = types.KeyboardButton('Расписание на текущую неделю')
-    itembtn7 = types.KeyboardButton('Расписание на следующую неделю')
-    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7)
-    bot.send_message(message.chat.id, "Выберите день недели:", reply_markup=markup)
+    itembtn1 = types.KeyboardButton('Какая неделя')
+    itembtn2 = types.KeyboardButton('Сайт')
+    itembtn3 = types.KeyboardButton('Помощь')
+    itembtn4 = types.KeyboardButton('Расписание')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    if message.text in ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']:
+def handle_all_message(message):
+    if message.text == 'Расписание':
+        markup = types.ReplyKeyboardMarkup(row_width=2)
+        itembtn1 = types.KeyboardButton('Понедельник')
+        itembtn2 = types.KeyboardButton('Вторник')
+        itembtn3 = types.KeyboardButton('Среда')
+        itembtn4 = types.KeyboardButton('Четверг')
+        itembtn5 = types.KeyboardButton('Пятница')
+        itembtn6 = types.KeyboardButton('Расписание на текущую неделю')
+        itembtn7 = types.KeyboardButton('Расписание на следующую неделю')
+        back_button = types.KeyboardButton('Назад')
+        markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7, back_button)
+        bot.send_message(message.chat.id, "Выберите день недели:", reply_markup=markup)
+    elif message.text in ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']:
         cur.execute(f"SELECT * FROM Timetable WHERE day='{message.text}'")
         rows = cur.fetchall()
         for row in rows:
@@ -46,24 +55,22 @@ def echo_all(message):
     elif message.text == 'Расписание на следующую неделю':
         # Добавьте логику для вывода расписания на следующую неделю
         pass
+    elif message.text == 'Какая неделя':
+        week_number = datetime.date.today().isocalendar()[1]
+        if week_number % 2 == 0:
+            bot.reply_to(message, 'Сейчас нижняя (Чётная) неделя.')
+        else:
+            bot.reply_to(message, 'Сейчас верхняя (Нечётная) неделя.')
+    elif message.text == 'Сайт':
+        bot.reply_to(message, 'https://mtuci.ru/')
+
+    elif message.text == 'Помощь':
+        bot.reply_to(message,
+                     'Этот бот предназначен для отображения расписания занятий. Вы можете выбрать день недели или показать расписание на текущую или следующую неделю. Список доступных команд: \n /start - начать работу с ботом \n /week - показать текущую неделю \n /mtuci - ссылка на официальный сайт МТУСИ \n /help - показать эту справку.')
+    elif message.text == 'Назад':
+        send_welcome(message)  # Возврат в главное меню
     else:
         bot.reply_to(message, 'Извините, я Вас не понял.')
 
-@bot.message_handler(commands=['week'])
-def send_week(message):
-    week_number = datetime.date.today().isocalendar()[1]
-    if week_number % 2 == 0:
-        bot.reply_to(message, 'Сейчас верхняя неделя.')
-    else:
-        bot.reply_to(message, 'Сейчас нижняя неделя.')
-
-    @bot.message_handler(commands=['mtuci'])
-    def send_mtuci(message):
-        bot.reply_to(message, 'https://mtuci.ru/')
-
-    @bot.message_handler(commands=['help'])
-    def send_help(message):
-        bot.reply_to(message,
-                     'Этот бот предназначен для отображения расписания занятий. Вы можете выбрать день недели или показать расписание на текущую или следующую неделю. Список доступных команд: \n /start - начать работу с ботом \n /week - показать текущую неделю \n /mtuci - ссылка на официальный сайт МТУСИ \n /help - показать эту справку.')
-
 bot.polling()
+

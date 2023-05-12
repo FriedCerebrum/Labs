2086e494ba2155bc234ef08185a89061e9ef354d
PotatoJisus <112276251+PotatoJisus@users.noreply.github.com>
2023-05-13 00:35:22 +0300

    4

--- lab6/calculator.py
+++ lab6/calculator.py
@@ -1,80 +1,67 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QPushButton, QMessageBox
-#QApplication – управляет потоком управления и основными настройками приложения с графическим интерфейсом
-#QWidget – является базовым классом для всех объектов пользовательского интерфейса
-#QLineEdit – виджет, который разрешает вводить и редактировать одну строку текста
-#QHBoxLayout – выстраивает виджеты по горизонтали
-#QVBoxLayout – выстраивает виджеты по вертикали
-#QPushButton – кнопка, на которую можно нажимать
-
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QGridLayout, QPushButton, QMessageBox
 
 class Calculator(QWidget):
     def __init__(self):
         super(Calculator, self).__init__()
-        #Создаем вертикальный слой и горизонтальные слои для размещения элементов
-        self.vbox = QVBoxLayout(self)
-        self.hbox_input = QHBoxLayout()
-        self.hbox_first = QHBoxLayout()
-        self.hbox_second = QHBoxLayout()
-        self.hbox_third = QHBoxLayout()
-        self.hbox_result = QHBoxLayout()
-        # Добавляем горизонтальные слои в вертикальный слой
-        self.vbox.addLayout(self.hbox_input)
-        self.vbox.addLayout(self.hbox_first)
-        self.vbox.addLayout(self.hbox_second)
-        self.vbox.addLayout(self.hbox_third)
-        self.vbox.addLayout(self.hbox_result)
+
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
+
         # Создаем поле ввода и добавляем его на соответствующий слой
         self.input = QLineEdit(self)
-        self.hbox_input.addWidget(self.input)
+        self.grid.addWidget(self.input, 0, 0, 1, 4)
+
         # Создаем кнопки с цифрами и добавляем их на соответствующие слои
         self.b_1 = QPushButton("1", self)
-        self.hbox_first.addWidget(self.b_1)
+        self.grid.addWidget(self.b_1, 1, 0)
 
         self.b_2 = QPushButton("2", self)
-        self.hbox_first.addWidget(self.b_2)
+        self.grid.addWidget(self.b_2, 1, 1)
 
         self.b_3 = QPushButton("3", self)
-        self.hbox_first.addWidget(self.b_3)
+        self.grid.addWidget(self.b_3, 1, 2)
 
         self.b_4 = QPushButton("4", self)
-        self.hbox_second.addWidget(self.b_4)
+        self.grid.addWidget(self.b_4, 2, 0)
 
         self.b_5 = QPushButton("5", self)
-        self.hbox_second.addWidget(self.b_5)
+        self.grid.addWidget(self.b_5, 2, 1)
 
         self.b_6 = QPushButton("6", self)
-        self.hbox_second.addWidget(self.b_6)
+        self.grid.addWidget(self.b_6, 2, 2)
 
         self.b_7 = QPushButton("7", self)
-        self.hbox_third.addWidget(self.b_7)
+        self.grid.addWidget(self.b_7, 3, 0)
 
         self.b_8 = QPushButton("8", self)
-        self.hbox_third.addWidget(self.b_8)
+        self.grid.addWidget(self.b_8, 3, 1)
 
         self.b_9 = QPushButton("9", self)
-        self.hbox_third.addWidget(self.b_9)
+        self.grid.addWidget(self.b_9, 3, 2)
 
         self.b_0 = QPushButton("0", self)
-        self.hbox_third.addWidget(self.b_0)
+        self.grid.addWidget(self.b_0, 4, 1)
 
         self.b_dot = QPushButton(".", self)
-        self.hbox_second.addWidget(self.b_dot)
+        self.grid.addWidget(self.b_dot, 4, 0)
+
         # Создаем кнопки с операторами и добавляем их на соответствующие слои
         self.b_plus = QPushButton("+", self)
-        self.hbox_result.addWidget(self.b_plus)
+        self.grid.addWidget(self.b_plus, 1, 3)
 
         self.b_minus = QPushButton("-", self)
-        self.hbox_result.addWidget(self.b_minus)
+        self.grid.addWidget(self.b_minus, 2, 3)
 
         self.b_multiply = QPushButton("*", self)
-        self.hbox_result.addWidget(self.b_multiply)
+        self.grid.addWidget(self.b_multiply, 3, 3)
 
         self.b_divide = QPushButton("/", self)
-        self.hbox_result.addWidget(self.b_divide)
+        self.grid.addWidget(self.b_divide, 4, 3)
 
         self.b_result = QPushButton("=", self)
-        self.hbox_result.addWidget(self.b_result)
+        self.grid.addWidget(self.b_result, 4, 2)
+
         # Подключаем обработчики событий для каждой кнопки
         self.b_1.clicked.connect(lambda: self._button("1"))
         self.b_2.clicked.connect(lambda: self._button("2"))
@@ -83,7 +70,7 @@ class Calculator(QWidget):
         self.b_5.clicked.connect(lambda: self._button("5"))
         self.b_6.clicked.connect(lambda: self._button("6"))
         self.b_7.clicked.connect(lambda: self._button("7"))
-        self.b_8.clicked.connect(lambda: self._button("8"))
+        self.b_8.clicked.connect(lambda: self.button("8"))
         self.b_9.clicked.connect(lambda: self._button("9"))
         self.b_0.clicked.connect(lambda: self._button("0"))
         self.b_dot.clicked.connect(lambda: self._button("."))
@@ -93,7 +80,7 @@ class Calculator(QWidget):
         self.b_divide.clicked.connect(lambda: self._button("/"))
         self.b_result.clicked.connect(self._calculate)
         # Устанавливаем параметры главного окна приложения
-        self.setGeometry(400, 400, 400, 400)
+        self.setGeometry(100, 100, 100, 100)
         self.setWindowTitle('Mega Duper Super Calculator v 0.1 alpha beta - trash')
         self.show()
 

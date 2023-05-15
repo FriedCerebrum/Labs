2061e1587343e82744e16fea11fcad50b194e055
PotatoJisus <112276251+PotatoJisus@users.noreply.github.com>
2023-05-15 23:54:12 +0300

    4

--- lab7/Simple-bot.py
+++ lab7/Simple-bot.py
@@ -41,15 +41,16 @@ def handle_all_message(message):
         itembtn3 = types.KeyboardButton('Среда')
         itembtn4 = types.KeyboardButton('Четверг')
         itembtn5 = types.KeyboardButton('Пятница')
-        itembtn6 = types.KeyboardButton('Расписание на текущую неделю')
-        itembtn7 = types.KeyboardButton('Расписание на следующую неделю')
+        itembtn6 = types.KeyboardButton('Суббота')
+        itembtn7 = types.KeyboardButton('Расписание на текущую неделю')
+        itembtn8 = types.KeyboardButton('Расписание на следующую неделю')
         back_button = types.KeyboardButton('Назад')
-        markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7, back_button)
+        markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6, itembtn7, itembtn8, back_button)
         bot.send_message(message.chat.id, "Выберите день недели:", reply_markup=markup)
 
-    elif message.text in ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']:
+    elif message.text in ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']:
         week_type = 1 if get_current_week_type() == 'верхняя' else 2
-        day_of_week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница'].index(message.text) + 1
+        day_of_week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'].index(message.text) + 1
 
         cur.execute(f"""
                 SELECT t.start_time, s.name, st.name, te.full_name, tt.room_number
@@ -74,22 +75,22 @@ def handle_all_message(message):
             bot.reply_to(message, "Расписание отсутствует")
 
     elif message.text in ['Расписание на текущую неделю', 'Расписание на следующую неделю']:
-        days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']
+        days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
         week_type = 1 if message.text == 'Расписание на текущую неделю' else 2
 
-        for day_of_week in range(1, 6):
+        for day_of_week in range(1, 7):
             cur.execute(f"""
-                        SELECT t.start_time, s.name, st.name, te.full_name, tt.room_number
-                        FROM timetable tt
-                        JOIN class c ON tt.class = c.id
-                        JOIN subject s ON c.subject = s.id
-                        JOIN subject_type st ON c.subject_type = st.id
-                        JOIN teacher_subject ts ON ts.class = c.id
-                        JOIN teacher te ON ts.teacher = te.id
-                        JOIN time t ON tt.class_time = t.id
-                        WHERE tt.week = {week_type} AND tt.day = {day_of_week}
-                        ORDER BY t.id
-                    """)
+                            SELECT t.start_time, s.name, st.name, te.full_name, tt.room_number
+                            FROM timetable tt
+                            JOIN class c ON tt.class = c.id
+                            JOIN subject s ON c.subject = s.id
+                            JOIN subject_type st ON c.subject_type = st.id
+                            JOIN teacher_subject ts ON ts.class = c.id
+                            JOIN teacher te ON ts.teacher = te.id
+                            JOIN time t ON tt.class_time = t.id
+                            WHERE tt.week = {week_type} AND tt.day = {day_of_week}
+                            ORDER BY t.id
+                        """)
             rows = cur.fetchall()
             if rows:
                 schedule = [f"{i + 1}. {row[0]}\n{row[1]}\n{row[2]}\n{row[3]} | {row[4]}\n—————————" for i, row in
@@ -121,4 +122,6 @@ def handle_all_message(message):
     else:
         bot.reply_to(message, 'Извините, я Вас не понял.')
 
+
 bot.polling()
+
